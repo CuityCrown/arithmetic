976b4758e1ecc47d555d471c611ef8d994cada9f
liuyb <liuybcrown@163.com>
2025-06-30 17:22:57 +0800

    计算方式

--- src/main/java/com/ryml/elementary/Calculate.java
+++ src/main/java/com/ryml/elementary/Calculate.java
@@ -1,6 +1,9 @@
 package com.ryml.elementary;
 
+import com.alibaba.fastjson2.JSONObject;
 import org.junit.Test;
+
+import java.util.*;
 
 /**
  * @Description // TODO: 这里填写类的详细描述
@@ -44,7 +47,7 @@ public class Calculate {
      */
     @Test
     public void theSumOfYwoNumbers() {
-        System.out.println(JSONObject);
+        System.out.println(JSONObject.toJSONString(theSumOfYwoNumbers2(new int[]{2, 7, 11, 15}, 9)));
     }
 
     /**
@@ -58,11 +61,69 @@ public class Calculate {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[]{nums[i], nums[j]};
+                    return new int[]{i, j};
                 }
             }
         }
         return null;
     }
+
+    /**
+     * 计算两数之和=target
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    private int[] theSumOfYwoNumbers2(int[] nums, Integer target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+
+    /**
+     * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+     * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+     * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+     * 解释：
+     * 在 strs 中没有字符串可以通过重新排列来形成 "bat"。
+     * 字符串 "nat" 和 "tan" 是字母异位词，因为它们可以重新排列以形成彼此。
+     * 字符串 "ate" ，"eat" 和 "tea" 是字母异位词，因为它们可以重新排列以形成彼此。
+     * 示例 2:
+     * 输入: strs = [""]
+     * 输出: [[""]]
+     * 示例 3:
+     * 输入: strs = ["a"]
+     * 输出: [["a"]]
+     * 提示：
+     * 1 <= strs.length <= 104
+     * 0 <= strs[i].length <= 100
+     * strs[i] 仅包含小写字母
+     */
+    @Test
+    public void group() {
+        Collection<List<String>> group = group(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+        System.out.println(JSONObject.toJSONString(group));
+    }
+
+    public Collection<List<String>> group(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return map.values();
+    }
+
 
 }
